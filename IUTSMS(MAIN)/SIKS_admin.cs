c0df26b3a565bfda33408d4387e8fe2eaba059b6
Shefwef <shefayatadib@iut-dhaka.edu>
2023-04-18 17:04:32 +0600

    Made SIKS admin

---
+++ IUTSMS(MAIN)/SIKS_admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IUTSMS_MAIN_
+{
+    public partial class SIKS_admin : Form
+    {
+        public SIKS_admin()
+        {
+            InitializeComponent();
+        }
+
+        private void SIKS_admin_Load(object sender, EventArgs e)
+        {
+            WinAPI.AnimateWindow(this.Handle, 500, WinAPI.BLEND);
+        }
+    }
+}
