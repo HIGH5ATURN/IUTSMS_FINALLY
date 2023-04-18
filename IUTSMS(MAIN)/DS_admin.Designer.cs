af9f7bcb8c9427d18de810b670ae6a644a39e2a4
KashshafLabib <kashshaflabib@iut-dhaka.edu>
2023-04-18 17:06:43 +0600

    DS and PS form transfers

---
+++ IUTSMS(MAIN)/DS_admin.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace IUTSMS_MAIN_
+{
+    partial class DS_admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DS_admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 510);
+            this.Name = "DS_admin";
+            this.Text = "DS_admin";
+            this.Load += new System.EventHandler(this.DS_admin_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
